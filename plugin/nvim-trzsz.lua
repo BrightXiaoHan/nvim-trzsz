d258eae21fc2a198451d18e36b7d8b72f0d9f9df
BrightXiaoHan <beatmight@gmail.com>
2025-01-14 16:06:44 +0800

    Remove redundant loaded check and streamline user command registration

--- plugin/nvim-trzsz.lua
+++ plugin/nvim-trzsz.lua
@@ -1,8 +1,3 @@
-if vim.g.loaded_nvim_trzsz then
-	return
-end
-vim.g.loaded_nvim_trzsz = true
-
 -- Register commands
-vim.api.nvim_create_user_command("NvimTreeTsz", require'nvim-trzsz'.nvim_tree_tsz, {})
-vim.api.nvim_create_user_command("NvimTreeTrz", require'nvim-trzsz'.nvim_tree_trz, {})
+vim.api.nvim_create_user_command("NvimTreeTsz", require('nvim-trzsz').nvim_tree_tsz, {})
+vim.api.nvim_create_user_command("NvimTreeTrz", require('nvim-trzsz').nvim_tree_trz, {})
