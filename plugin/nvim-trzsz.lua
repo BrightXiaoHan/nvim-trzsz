c0b6bf604a885c8ff9e393307f2b8ea9503607e9
BrightXiaoHan <beatmight@gmail.com>
2025-01-14 15:29:03 +0800

    Enhance tsz command check and migrate user commands to nvim_create_user_command

--- plugin/nvim-trzsz.lua
+++ plugin/nvim-trzsz.lua
@@ -4,3 +4,5 @@ end
 vim.g.loaded_nvim_trzsz = true
 
 -- Add any vim commands or autocommands here
+vim.api.nvim_create_user_command("NvimTreeTsz", "lua require'nvim-trzsz'.nvim_tree_tsz()", { nargs = 0 })
+vim.api.nvim_create_user_command("NvimTreeTrz", "lua require'nvim-trzsz'.nvim_tree_trz()", { nargs = 0 })
